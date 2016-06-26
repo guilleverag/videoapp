eb922e334ac0d2b5c76ece1aaebebf3bb13691a8
guilleverag <guilleverag@gmail.com>
2016-06-26 15:28:03 -0400

    video preview

--- www/js/videoControllers.js
+++ www/js/videoControllers.js
@@ -1,14 +1,14 @@
 var videoControllers = angular.module('videoControllers',[])
 
-.controller("VideoCtrl", function($scope, $cordovaCapture, videoServices) {
-    $scope.clip = '';
+.controller("VideoCtrl", function($scope, $ionicModal, $cordovaCapture, videoServices) {
+    $scope.video = '';
  
     $scope.captureVideo = function() {
         var options = { limit: 1, duration: 10 };
         
         $cordovaCapture.captureVideo(options).then(function(videoData) {
             videoServices.saveVideo(videoData).success(function(data) {
-                $scope.clip = data;
+                $scope.video = data;
                 $scope.$apply();
             }).error(function(data) {
                 console.log('ERROR: ' + data);
@@ -16,16 +16,30 @@ var videoControllers = angular.module('videoControllers',[])
         });
     };
     
-    $scope.urlForClipThumb = function(clipUrl) {
-        var name = clipUrl.substr(clipUrl.lastIndexOf('/') + 1);
+    $scope.urlForVideoThumb = function(videoUrl) {
+        var name = videoUrl.substr(clipUrl.lastIndexOf('/') + 1);
         var trueOrigin = cordova.file.dataDirectory + name;
         var sliced = trueOrigin.slice(0, -4);
         return sliced + '.png';
     }
-     
-    $scope.showClip = function(clip) {
-        console.log('show clip: ' + clip);
-    }
+    
+    $scope.showVideo = function(video) {
+        $scope.videoSrc  = video;
+        
+		$ionicModal.fromTemplateUrl('templates/video.html', {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.modal = modal;
+			$scope.modal.show();
+		});
+	}
+ 
+	// Close the modal
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+		$scope.modal.remove()
+	};
  
 })
 
